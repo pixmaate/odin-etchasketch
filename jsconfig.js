d35fdd91c8e0421def1865974e62504264b8c4fb
PixMate <94467160+pixmaate@users.noreply.github.com>
2023-10-16 20:51:36 +0200

    reset button implemented

--- jsconfig.js
+++ jsconfig.js
@@ -1,10 +1,16 @@
 const boxHolder = document.querySelector('.contentHolder');
+const resetButton = document.querySelector('.resetBtn');
+
+resetButton.addEventListener('click', () =>{
+    resetGrid();
+});
 
 function makeBoxes(numberOfBoxes) {
     for (let i=1; i<= (numberOfBoxes**2); i++) {
         const divBox = document.createElement('div');
         let sizeValue = 480/numberOfBoxes;
         sizeValue = sizeValue + 'px'
+        divBox.classList.toggle('oneBox')
         divBox.style.cssText = `width: ${sizeValue}; height: ${sizeValue}; border-style: solid; box-sizing: border-box; border-width: 1px`;
         boxHolder.appendChild(divBox);
     };
@@ -19,6 +25,13 @@ boxHolder.addEventListener('mouseover', (event) => {
     }
     event.target.style.backgroundColor = 'black';
 });
+
+function resetGrid() {
+    const everyBox = document.querySelectorAll('.oneBox');
+    everyBox.forEach(el => {
+        el.style.backgroundColor = 'white';
+    });
+}
 
 
 makeBoxes(10);
