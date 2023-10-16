1fae9e234b942c2c446d0ddd3f4286e5f62abf29
PixMate <94467160+pixmaate@users.noreply.github.com>
2023-10-16 20:14:53 +0200

    can create one box

--- jsconfig.js
+++ jsconfig.js
@@ -1,2 +1,11 @@
-const divBox = document.createElement('div');
+const boxHolder = document.querySelector('.contentHolder');
 
+function makeBoxes(numberOfBoxes) {
+    const divBox = document.createElement('div');
+    let sizeValue = 480/numberOfBoxes;
+    sizeValue = sizeValue + 'px'
+    divBox.style.cssText = `width: ${sizeValue}; height: ${sizeValue}; border-style: solid; box-sizing: border-box; border-width: 2px`;
+    boxHolder.appendChild(divBox);
+}
+
+makeBoxes(24);
