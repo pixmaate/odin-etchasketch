9a6f4673e1b2eee1683dbba73eb2e54357fea2f5
PixMate <94467160+pixmaate@users.noreply.github.com>
2023-10-16 20:18:26 +0200

    can create full boxlist

--- jsconfig.js
+++ jsconfig.js
@@ -1,11 +1,14 @@
 const boxHolder = document.querySelector('.contentHolder');
 
 function makeBoxes(numberOfBoxes) {
-    const divBox = document.createElement('div');
-    let sizeValue = 480/numberOfBoxes;
-    sizeValue = sizeValue + 'px'
-    divBox.style.cssText = `width: ${sizeValue}; height: ${sizeValue}; border-style: solid; box-sizing: border-box; border-width: 2px`;
-    boxHolder.appendChild(divBox);
-}
+    for (let i=1; i<= (numberOfBoxes**2); i++) {
+        const divBox = document.createElement('div');
+        let sizeValue = 480/numberOfBoxes;
+        sizeValue = sizeValue + 'px'
+        divBox.style.cssText = `width: ${sizeValue}; height: ${sizeValue}; border-style: solid; box-sizing: border-box; border-width: 1px`;
+        boxHolder.appendChild(divBox);
+    };
+    
+};
 
 makeBoxes(24);
